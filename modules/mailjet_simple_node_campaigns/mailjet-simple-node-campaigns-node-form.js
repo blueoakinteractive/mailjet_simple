164c95d76d2b70071ef81e91736653d5442f0d5a
Andy Giles <andyg5000@gmail.com>
2017-03-08 11:22:15 -0500

    Created mailjet_simple_node_campaigns

---
+++ modules/mailjet_simple_node_campaigns/mailjet-simple-node-campaigns-node-form.js
@@ -0,0 +1,17 @@
+
+(function ($) {
+
+Drupal.behaviors.mailjetFieldsetSummaries = {
+  attach: function (context) {
+    $('fieldset.mailjet-simple-node-campaigns-settings', context).drupalSetSummary(function (context) {
+      if ($('.form-item-create-and-send input', context).is(':checked')) {
+        return Drupal.t('Campaign will be sent on node save');
+      }
+      else {
+        return Drupal.t('Disabled');
+      }
+    });
+  }
+};
+
+})(jQuery);
